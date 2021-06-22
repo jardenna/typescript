a291c29749ec59cf2396dc203a42b762a0dde5f3
jardenna <hbjornum@gmail.com>
2021-06-22 15:08:26 +0200

    test

--- src/features/postSlice.ts
+++ src/features/postSlice.ts
@@ -1,6 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import type { RootState } from '../app/store';
-
 import { AppDispatch } from '../app/store';
 //import fetchApi from './fetchApi';
 
