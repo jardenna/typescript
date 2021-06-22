51c5068662de4f9c859ff043c05017f185e433ba
jardenna <hbjornum@gmail.com>
2021-06-22 12:28:26 +0200

    Test

--- src/app/store.ts
+++ src/app/store.ts
@@ -13,7 +13,6 @@ const store = configureStore({
    }
 });
 
-
 export default store;
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
